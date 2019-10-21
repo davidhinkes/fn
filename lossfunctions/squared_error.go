4401077b904afebec93cb1a09b3a4fdec289beab
David Hinkes <david.hinkes@gmail.com>
2019-10-21 03:28:24 +0000

    Added arena support for squared_error loss function.

--- lossfunctions/squared_error.go
+++ lossfunctions/squared_error.go
@@ -2,27 +2,28 @@ package lossfunctions
 
 import (
 	"fn"
+	arena "fn/internal"
 
 	"gonum.org/v1/gonum/mat"
 )
 
 func NewSquaredError() fn.LossFunction {
-	return squaredError{}
+	return squaredError{
+		arena: arena.Make(),
+	}
 }
 
 type squaredError struct {
+	arena arena.T
 }
 
 func (s squaredError) F(y mat.Vector, yHat mat.Vector) (float64, mat.Vector) {
-	e := &mat.VecDense{}
+	s.arena.Reset()
+	e, _ := s.arena.NewVecDense(y.Len())
 	e.SubVec(y,yHat)
-	e.MulElemVec(e,e)
-	return mat.Sum(e), s.d(y, yHat)
-}
-
-func (s squaredError) d(y mat.Vector, yHat mat.Vector) mat.Vector {
-	e := &mat.VecDense{}
-	e.SubVec(y, yHat)
-	e.ScaleVec(2.0, e)
-	return e
+	loss, _ := s.arena.NewVecDense(y.Len())
+	loss.MulElemVec(e,e)
+	d, _ := s.arena.NewVecDense(y.Len())
+	d.ScaleVec(2.0, e)
+	return mat.Sum(loss), e
 }
