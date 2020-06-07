81db2851cfab089f97f39cf4fa82e771c59f39e7
David Hinkes <david.hinkes@gmail.com>
2020-06-07 10:42:52 -0700

    remove arena from squared_error
    This makes the package thread safe

--- lossfunctions/squared_error.go
+++ lossfunctions/squared_error.go
@@ -2,28 +2,23 @@ package lossfunctions
 
 import (
 	"fn"
-	arena "fn/internal"
 
 	"gonum.org/v1/gonum/mat"
 )
 
 func NewSquaredError() fn.LossFunction {
-	return squaredError{
-		arena: arena.Make(),
-	}
+	return squaredError{}
 }
 
 type squaredError struct {
-	arena arena.T
 }
 
 func (s squaredError) F(y mat.Vector, yHat mat.Vector) (float64, mat.Vector) {
-	s.arena.Reset()
-	e, _ := s.arena.NewVecDense(y.Len())
+	e := mat.NewVecDense(y.Len(), nil)
 	e.SubVec(y, yHat)
-	loss, _ := s.arena.NewVecDense(y.Len())
+	loss := mat.NewVecDense(y.Len(), nil)
 	loss.MulElemVec(e, e)
-	d, _ := s.arena.NewVecDense(y.Len())
+	d := mat.NewVecDense(y.Len(), nil)
 	d.ScaleVec(2.0, e)
 	return mat.Sum(loss), d
 }
