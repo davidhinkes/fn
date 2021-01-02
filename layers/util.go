2b68ab97f90d0610399a52f58dc8b394230547dc
David Hinkes <david.hinkes@gmail.com>
2021-01-01 21:34:26 -0800

    removed non-used function

--- layers/util.go
+++ layers/util.go
@@ -1,17 +1,8 @@
 package layers
 
 import (
-	"math/rand"
-
 	"gonum.org/v1/gonum/mat"
 )
-
-func randomize(v *mat.VecDense) {
-	for i := 0; i < v.Len(); i++ {
-		r := 2*rand.Float64() - 1
-		v.SetVec(i, r)
-	}
-}
 
 func diag(x mat.Vector) *mat.DiagDense {
 	ret := mat.NewDiagDense(x.Len(), nil)
