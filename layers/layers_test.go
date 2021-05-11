74e9c96bf6882d9db69a5124371e9308aeb50921
David Hinkes <david.hinkes@gmail.com>
2021-05-11 13:01:43 -0700

    added training test for static functions

--- layers/layers_test.go
+++ layers/layers_test.go
@@ -12,15 +12,15 @@ import (
 	"gonum.org/v1/gonum/mat"
 )
 
-func testLayer(t *testing.T, mkLayer func(int) fn.Layer, truth test.Truth) {
+func testLayer(t *testing.T, mkModel func(int) fn.Model, truth test.Truth) {
 	t.Helper()
 	n, _ := truth.Dims()
-	model := fn.MakeModel(mkLayer(n))
+	model := mkModel(n)
 	startTime := time.Now()
 	const (
 		batchSize       = 2048
 		alpha           = 5e-2
-		durationSeconds = 20
+		durationSeconds = 15
 	)
 	var e float64
 	lossFunction := lossfunctions.NewSquaredError()
@@ -32,21 +32,29 @@ func testLayer(t *testing.T, mkLayer func(int) fn.Layer, truth test.Truth) {
 		}
 	}
 	t.Logf("%s error=%v", t.Name(), e)
-	if e > 1e-5 {
+	if e > 1e-4 {
 		t.Errorf("error rate too high (e=%v)", e)
 	}
 }
 
 func TestBiasLayer(t *testing.T) {
-	testLayer(t, func(n int) fn.Layer { return MakeBiasLayer(n) }, identity{N: 64})
+	testLayer(t, func(n int) fn.Model { return fn.MakeModel(MakeBiasLayer(n)) }, identity{N: 64})
 }
 
 func TestPerceptronLayer(t *testing.T) {
-	testLayer(t, func(n int) fn.Layer { return MakePerceptronLayer(n, n) }, identity{N: 8})
+	testLayer(t, func(n int) fn.Model{ return fn.MakeModel(MakePerceptronLayer(n, n)) }, identity{N: 8})
 }
 
 func TestScalarLayer(t *testing.T) {
-	testLayer(t, func(n int) fn.Layer { return MakeScalarLayer(n) }, identity{N: 64})
+	testLayer(t, func(n int) fn.Model{ return fn.MakeModel(MakeScalarLayer(n)) }, identity{N: 64})
+}
+
+func TestStaticFuncLayer(t *testing.T) {
+	f := func(x float64) float64 { return -2*x }
+	d := func(x float64) float64 { return -2 }
+	testLayer(t, func(n int) fn.Model{
+		return fn.MakeModel(MakePerceptronLayer(n, n), staticFunc{f: f, d: d}) },
+		identity{N: 8})
 }
 
 type identity struct {
