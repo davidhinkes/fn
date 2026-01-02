ba162fc7bbdd89dd7103b5cb26054cc9899b2964
David Hinkes <david.hinkes@gmail.com>
2026-01-01 21:12:56 -0800

    Initial attempt at recusive means of serial function joining.

--- serial.go
+++ serial.go
@@ -7,6 +7,13 @@ import (
 // Serial returns a single layer from multiple layers executed one after another.
 // TODO: I bet we can re-think this to be recursive. This would be quite elegant and simple.
 func Serial(layers ...Layer) Layer {
+	if len(layers) == 1 {
+		return layers[0]
+	}
+	return ser2{
+		left:  layers[0],
+		right: Serial(layers[1:]...),
+	}
 	var n int
 	for _, l := range layers {
 		n += l.NumWeights()
@@ -16,6 +23,46 @@ func Serial(layers ...Layer) Layer {
 		layers:     layers,
 		numWeights: n,
 	}
+}
+
+type ser2 struct {
+	left  Layer
+	right Layer
+}
+
+func (s ser2) NumWeights() int {
+	return s.left.NumWeights() + s.right.NumWeights()
+}
+
+func (s ser2) F(x mat.Vector, h []float64) mat.Vector {
+	n := s.left.NumWeights()
+	xPrime := s.left.F(x, h[:n])
+	return s.right.F(xPrime, h[n:])
+}
+
+func (s ser2) D(x mat.Vector, h []float64) (mat.Matrix, mat.Matrix) {
+	n := s.left.NumWeights()
+	dYdX, dYdAleph := s.left.D(x, h[:n])
+	y := s.left.F(x, h[:n])
+	dZdY, dZdBet := s.right.D(y, h[n:])
+	var dZdX mat.Dense
+	dZdX.Mul(dZdY, dYdX)
+	// it's possible that dZdAleph and/or dZdBet are nil
+	if dYdAleph == nil && dZdBet == nil {
+		return &dZdX, nil
+	}
+	if dYdAleph == nil {
+		return &dZdX, dZdBet
+	}
+	var dZdAleph mat.Dense
+	dZdAleph.Mul(dZdY, dYdAleph)
+	if dZdBet == nil {
+		return &dZdX, &dZdAleph
+	}
+	var dZdH mat.Dense
+	dZdH.Augment(&dZdAleph, dZdBet)
+
+	return &dZdX, &dZdH
 }
 
 type ser struct {
