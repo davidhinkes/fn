a7572b3118ae3fa21b8c86ce25e88e4a3399b00e
David Hinkes <david.hinkes@gmail.com>
2026-01-05 12:14:14 -0800

    rename of ser2 struct to serialNode

--- serial.go
+++ serial.go
@@ -9,31 +9,31 @@ func Serial(layers ...Layer) Layer {
 	if len(layers) == 1 {
 		return layers[0]
 	}
-	return ser2{
+	return serialNode{
 		left:  layers[0],
 		right: Serial(layers[1:]...),
 	}
 }
 
-// Type ser2 is the serial type, that adheres to the Layer interface.
+// Type serialNode is the core struct that implements a serial Layer.
 // It is implemented via recursion, which is elegant but perhaps has performance
 // concerns.
-type ser2 struct {
+type serialNode struct {
 	left  Layer
 	right Layer
 }
 
-func (s ser2) NumWeights() int {
+func (s serialNode) NumWeights() int {
 	return s.left.NumWeights() + s.right.NumWeights()
 }
 
-func (s ser2) F(x mat.Vector, h []float64) mat.Vector {
+func (s serialNode) F(x mat.Vector, h []float64) mat.Vector {
 	n := s.left.NumWeights()
 	xPrime := s.left.F(x, h[:n])
 	return s.right.F(xPrime, h[n:])
 }
 
-func (s ser2) D(x mat.Vector, h []float64) (mat.Matrix, mat.Matrix) {
+func (s serialNode) D(x mat.Vector, h []float64) (mat.Matrix, mat.Matrix) {
 	// [ℵ, ℶ] = h
 	// y = left(x, ℵ)
 	// z = right(y, ℶ)
@@ -47,7 +47,7 @@ func (s ser2) D(x mat.Vector, h []float64) (mat.Matrix, mat.Matrix) {
 	y := s.left.F(x, h[:n])
 	dZdY, dZdℶ := s.right.D(y, h[n:])
 	var dZdX mat.Dense
-	dZdX.Mul(dZdY, dYdX)
+	dZdX.Mul(dZdY, dYdX) // memory allocation
 	// it's possible that dZdℵ and/or dZdℶ are nil
 	if dYdℵ == nil && dZdℶ == nil {
 		return &dZdX, nil
@@ -56,12 +56,12 @@ func (s ser2) D(x mat.Vector, h []float64) (mat.Matrix, mat.Matrix) {
 		return &dZdX, dZdℶ
 	}
 	var dZdℵ mat.Dense
-	dZdℵ.Mul(dZdY, dYdℵ)
+	dZdℵ.Mul(dZdY, dYdℵ) // memory allocation
 	if dZdℶ == nil {
 		return &dZdX, &dZdℵ
 	}
 	var dZdH mat.Dense
-	dZdH.Augment(&dZdℵ, dZdℶ)
+	dZdH.Augment(&dZdℵ, dZdℶ) // not sure if this is an allocation or not
 
 	return &dZdX, &dZdH
 }
